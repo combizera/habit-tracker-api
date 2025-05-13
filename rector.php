bbe9e870bc1c97e069dd6f5f61eb8ee08ff1422c
combizera <ycombi349@gmail.com>
2025-05-13 18:45:50 -0300

    wip: pint

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/bootstrap/app.php',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     ->withPreparedSets(
         deadCode: true,
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn(): array => [config('app.name')]);
+Route::get('/', fn (): array => [config('app.name')]);
