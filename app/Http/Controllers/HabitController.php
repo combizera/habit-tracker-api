81ca1c734e8d6ac542f4c677fc5eb2288984e6c4
combizera <ycombi349@gmail.com>
2025-05-26 12:04:29 -0300

    feat: create first route

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreHabitRequest;
+use App\Http\Resources\HabitResource;
 use App\Models\Habit;
 use Illuminate\Http\Request;
 
 class HabitController extends Controller
 {
+    public function index()
+    {
+        return HabitResource::collection(
+            Habit::all()
+        );
+    }
+
     public function store(StoreHabitRequest $request)
     {
         $data = $request->validated();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HabitController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn (): array => [config('app.name')]);
+Route::prefix('/api')->name('api.')->group(function () {
+   Route::get('/habits', [HabitController::class, 'index'])->name('habits.index');
+});
