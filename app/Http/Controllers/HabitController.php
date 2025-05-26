7ecee097a4bc4c616c1668afd8a096404a6763a2
combizera <ycombi349@gmail.com>
2025-05-26 11:49:24 -0300

    feat: add HabitController, HabitResource, and StoreHabitRequest for habit management

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHabitRequest;
+use App\Models\Habit;
+use Illuminate\Http\Request;
+
+class HabitController extends Controller
+{
+    public function store(StoreHabitRequest $request)
+    {
+        $data = $request->validated();
+
+        $habit = Habit::query()->create($data);
+
+        return HabitResource::make($habit);
+    }
+}
